a7c448e70fd4b0623f76778d987e65aa0cecf7f0
devinheinzler <devinheinzler271@gmail.com>
2022-09-07 21:32:03 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(Ralph) {
+    cats.push(Ralph);
+} 
+
+function destructivelyPrependCat(Ralph) {
+    cats.unshift(Ralph);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    const copyOfCats = [...cats, name];
+    return copyOfCats;
+}
+
+function prependCat(name) {
+    const anotherCopyOfCats = [name, ...cats];
+    return anotherCopyOfCats;
+}
+
+function removeLastCat() {
+    const copyingIsTedious = cats.slice(0, -1);
+    return copyingIsTedious;
+}
+
+function removeFirstCat() {
+    const copyingIsntTedious = cats.slice(1);
+    return copyingIsntTedious;
+}
